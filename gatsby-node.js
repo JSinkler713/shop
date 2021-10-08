23b31354b1eeff261e6c97e3c13230182c012419
James Sinkler <jamessinkler713@gmail.com>
2021-10-08 14:43:09 -0700

    dynamically create product pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,51 @@
  */
 
 // You can delete this file if you're not using it
+const path = require(`path`)
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  // Query for all products in Shopify
+  const result = await graphql(`
+    query {
+      allShopifyProduct(sort: { fields: [title] }) {
+        edges {
+          node {
+            title
+            featuredImage {
+								gatsbyImageData(width: 200)
+            }
+            variants {
+              shopifyId
+              image {
+                gatsbyImageData(width: 200)
+              }
+              productId
+            }
+            handle
+            description
+            priceRangeV2 {
+              maxVariantPrice {
+                amount
+              }
+              minVariantPrice {
+                amount
+              }
+            }
+            status
+          }
+        }
+      }
+    }
+  `)
+  // Iterate over all products and create a new page using a template
+  // The product "handle" is generated automatically by Shopify
+  result.data.allShopifyProduct.edges.forEach(({ node }) => {
+    createPage({
+      path: `/products/${node.handle}`,
+      component: path.resolve(`./src/templates/product.js`),
+      context: {
+        product: node,
+      },
+    })
+  })
+}
