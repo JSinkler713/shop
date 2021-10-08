b64854bf5fab6f22ebfd082b9bf2af8d23e0267c
James Sinkler <jamessinkler713@gmail.com>
2021-10-08 14:39:30 -0700

    context set up to addToCart

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,10 @@
  */
 
 // You can delete this file if you're not using it
+import * as React from "react"
+import { StoreProvider } from "./src/context/store-context"
+
+//wrap pages with store-context
+export const wrapRootElement = ({ element }) => (
+  <StoreProvider>{element}</StoreProvider>
+)
